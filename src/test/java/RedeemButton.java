2b81738813a1982e5e9a5de6e4b9bc8e7898e3db
Asus <uladzislau.krasouski@pvls.tech>
2023-04-12 18:06:47 +0300

    Third try

--- src/test/java/RedeemButton.java
+++ src/test/java/RedeemButton.java
@@ -48,7 +48,7 @@ public class RedeemButton extends data{
     @Test
     public void redeemButton() throws InterruptedException {
         List<WebElement> inputLogin = driver.findElements(By.xpath("//input[@step='any']"));
-        inputLogin.get(0).sendKeys(notValidLogin); /////
+        inputLogin.get(0).sendKeys(login);
 
         List<WebElement> inputPassword = driver.findElements(By.xpath("//input[@step='any']"));
         inputPassword.get(1).sendKeys(password);
